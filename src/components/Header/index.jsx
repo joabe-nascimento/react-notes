cf761b8549ebd8f3f0b0dd52cda4a7e315b6ec27
Joabe Nascimento <joabefnascimento1@outlook.com>
2024-04-05 23:54:50 -0300

    tela header finalizado

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import { RiShutDownLine } from "react-icons/ri";
+import { Container, Profile, Logout } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img
+          src="https://avatars.githubusercontent.com/u/63386616?v=4"
+          alt="Joabe Nascimento"
+        />
+        <div>
+          <span>Bem-Vindo</span>
+          <strong>Joabe Nascimento</strong>
+        </div>
+      </Profile>
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  );
+}
