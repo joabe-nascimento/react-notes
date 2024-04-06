054943a0257ef9cd438536ca5afba80bc507fa2a
Joabe Nascimento <joabefnascimento1@outlook.com>
2024-04-06 15:37:37 -0300

    Aplicando Grid na Home

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,41 @@
-import { Container } from "./styles";
+import { Container, Links, Content } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { ButtonText } from "../../components/ButtonText";
+import { Tag } from "../../components/Tags";
 
 export function Details() {
   return (
     <Container>
       <Header />
-      <Button title="Voltar" />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+          <h1>Introdução a ReactJs</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia
+            impedit, ipsam cumque quibusdam quod et ea iste, hic eius voluptatum
+            illo cum ad? Corrupti deserunt consequatur vero. Asperiores,
+            excepturi tempora.
+          </p>
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
